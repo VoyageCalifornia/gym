f30611a3cb4afd44d4f6e30c6fa0669d45da8fc2
Voyage California <dumusednote@gmail.com>
2025-01-11 14:15:39 +0200

    new blocks

---
+++ main.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".hero-swiper", {
+    initialSlide: 2,
+    spaceBetween: 50,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+var swiper = new Swiper(".secondary-slider", {
+    initialSlide: 2,
+    spaceBetween: 50,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
